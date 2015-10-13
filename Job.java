037acc4cdd78b877c20766e8e8e21132e36465a9
George Devine <gmd210@exeter.ac.uk>
2015-10-13 16:08:52 +0100

    added doc comments to MyTak

--- Job.java
+++ Job.java
@@ -1,3 +1,10 @@
+/**
+ * This class simulates a task that a thread must complete
+ * 
+ * @author George Devine
+ * @version 13/10/2015
+ */
+
 public class Job implements Runnable
 {
   private int jobNumber;
@@ -8,13 +15,13 @@ public class Job implements Runnable
   public void run()
   {
     System.out.println("Job: " + jobNumber + " is being processed by thread : " + Thread.currentThread().getName());
-  }
-  try
-  {
-    Thread.sleep((int) (1000))
-  }
-  catch (InterruptedClass e)
-  {
-  }
-  System.out.println("Job: " + jobNumber + " is ending in thread : " + Thread.currentThread().getName());
+      try
+      {
+        Thread.sleep((int) (1000));
+      }
+      catch (InterruptedException e)
+      {
+      }
+      System.out.println("Job: " + jobNumber + " is ending in thread : " + Thread.currentThread().getName());
+    }
 }
